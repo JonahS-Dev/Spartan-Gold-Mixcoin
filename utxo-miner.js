6c10d975e0d97a7806ee94de1ecee4f5d376a31b
devintaing <devin.taing@sjsu.edu>
2026-04-23 22:23:57 -0700

    homework 2 files

---
+++ utxo-miner.js
@@ -0,0 +1,71 @@
+"use strict";
+
+const { Miner } = require("spartan-gold");
+
+const UtxoMixin = require("./utxo-mixin.js");
+
+/**
+ * A UtxoMiner is capable of creating multiple addresses,
+ * and will create a new address for any mining rewards.
+ */
+module.exports = class UtxoMiner extends Miner {
+
+  /**
+   * Extends miner with UTXO support, including setting up a wallet.
+   * 
+   * @constructor
+   * @param  {...any} args - Arguments needed for Miner constructor.
+   */
+  constructor(...args) {
+    super(...args);
+
+    // Mixing in support for UTXOs.
+    Object.assign(this, UtxoMixin);
+
+    this.setupWallet();
+  }
+
+  /**
+   * Gets balance of miner from last confirmed block,
+   * not counting any pending ingoing or outgoing transactions.
+   * 
+   * @returns {number} -- Total available gold as of the last confirmed block.
+   */
+  get confirmedBalance() {
+    return this.getConfirmedBalance();
+  }
+
+  /**
+   * Sets up the miner to start searching for a new block.
+   * 
+   * @param {Set} [txSet] - Transactions the miner has that have not been accepted yet.
+   */
+  startNewSearch(...args) {
+
+    // Check to see if the miner has already received a reward at this address
+    // (or has a starting balance at this address). If so, set up a new one.
+    //
+    // To do this, you will need to compare the lastBlock's rewardAddr field against
+    // the miner's current address, and to make sure that the address does not already
+    // have a balance of gold associated with it.
+
+    //
+    // **YOUR CODE HERE**
+    //
+
+    // check if the miner has already received a reward at this address
+    let gotRewardPreviously = this.lastBlock.rewardAddr === this.address;
+
+    // check if the miner already has a starting balance at this address
+    let hasStartingBalance = this.lastBlock.balanceOf(this.address) > 0;
+
+    // if either of the above are true, set up a new address
+    if (gotRewardPreviously || hasStartingBalance) {
+      this.createAddress();
+    }
+
+
+    super.startNewSearch(...args);
+  }
+
+}
