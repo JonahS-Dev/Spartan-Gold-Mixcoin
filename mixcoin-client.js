2992acfdc1246d64830a0664c95d331cecb4eb53
devintaing <devin.taing@sjsu.edu>
2026-04-24 13:18:39 -0700

    Add mixcoin-client.js to start adding mixcoin client behavior

---
+++ mixcoin-client.js
@@ -0,0 +1,28 @@
+"use strict";
+
+const UtxoClient = require("./utxo-client.js");
+
+/**
+ * Add Mixcoin client behavior on top of the UTXO client
+ */
+module.exports = class MixcoinClient extends UtxoClient {
+  constructor(...args) {
+    super(...args);
+
+    // store submitted mixing requests
+    this.pendingMixRequests = new Map();
+
+    // store promises/warranties from mixers to use as evidence
+    this.warranties = new Map();
+  }
+
+  // remember sent mixing requests to see the outcome later
+  rememberPendingMixRequest(request) {
+    this.pendingMixRequests.set(request.requestId, request);
+  }
+
+  // store warranties from mixers to use as evidence in the future
+  rememberWarranty(warranty) {
+    this.warranties.set(warranty.requestId, warranty);
+  }
+};
